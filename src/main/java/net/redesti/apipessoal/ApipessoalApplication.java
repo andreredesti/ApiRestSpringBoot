c38acc7aabd25dc5641b22e0aaa30eacceae6a8f
Andre Nascimento <andreredesti@gmail.com>
2021-06-22 20:57:03 -0500

    Adicionando o Projeto ApiRestSpringBoot no Git

---
+++ src/main/java/net/redesti/apipessoal/ApipessoalApplication.java
@@ -0,0 +1,13 @@
+package net.redesti.apipessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipessoalApplication.class, args);
+	}
+
+}
